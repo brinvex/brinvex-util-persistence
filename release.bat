1110ccf76a9c457e888fc0b74c1543601b37fd22
Lukas <lr@brinvex.com>
2023-11-05 15:02:20 +0100

    feat: release.bat

---
+++ release.bat
@@ -0,0 +1,21 @@
+set JAVA_HOME="C:\tools\java\jdk-21.0.1"
+set MVN_HOME="C:\tools\mvn\mvn-3.8.7"
+set MVN=%MVN_HOME%\bin\mvn.exe
+
+REM Merge feature branch into master
+REM Update version in README
+REM Commit (dont push)
+
+call %MVN% clean package
+
+call %MVN% -P release clean license:format
+
+REM Amend commit if there are some changes
+
+call %MVN% -P release -Darguments=-DskipTests release:clean release:prepare
+
+git push
+
+call %MVN% -P release -Darguments=-DskipTests release:perform
+
+echo Continue on https://s01.oss.sonatype.org/
