768e4126f071c8083d9b6e93b1ca33831d2a9589
Lukas <lr@brinvex.com>
2023-11-05 15:06:30 +0100

    feat: release.bat

--- release.bat
+++ release.bat
@@ -1,12 +1,11 @@
 set JAVA_HOME="C:\tools\java\jdk-21.0.1"
-set MVN_HOME="C:\tools\mvn\mvn-3.8.7"
-set MVN=%MVN_HOME%\bin\mvn.exe
+set MVN=C:\tools\mvn\mvn-3.8.7\bin\mvn
 
 REM Merge feature branch into master
 REM Update version in README
 REM Commit (dont push)
 
-call %MVN% clean package
+REM call %MVN% clean package
 
 call %MVN% -P release clean license:format
 
@@ -14,8 +13,8 @@ REM Amend commit if there are some changes
 
 call %MVN% -P release -Darguments=-DskipTests release:clean release:prepare
 
-git push
+REM git push
 
-call %MVN% -P release -Darguments=-DskipTests release:perform
+REM call %MVN% -P release -Darguments=-DskipTests release:perform
 
-echo Continue on https://s01.oss.sonatype.org/
+REM echo Continue on https://s01.oss.sonatype.org/
